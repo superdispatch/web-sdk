96407d8898022ed4ee68700b7571b2b4fa5aca6b
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2019-12-11 11:00:57 +0500

    refactor: Use `Map` instead of `switch`.

--- src/PaymentMethod.ts
+++ src/PaymentMethod.ts
@@ -8,38 +8,21 @@ export type PaymentMethod =
   | 'direct_deposit'
   | 'other';
 
+const values = new Map<PaymentMethod, string>([
+  ['cash', 'Cash'],
+  ['check', 'Check'],
+  ['cashiers_check', 'Cashiers Check'],
+  ['money_order', 'Money Order'],
+  ['comchek', 'Comchek'],
+  ['credit_card', 'Credit Card'],
+  ['direct_deposit', 'Direct Deposit'],
+  ['other', 'Other'],
+]);
+
 export function listPaymentMethods(): PaymentMethod[] {
-  return [
-    'cash',
-    'check',
-    'cashiers_check',
-    'money_order',
-    'comchek',
-    'credit_card',
-    'direct_deposit',
-    'other',
-  ];
+  return Array.from(values.keys());
 }
 
 export function formatPaymentMethod(value: PaymentMethod): string {
-  switch (value) {
-    case 'cash':
-      return 'Cash';
-    case 'check':
-      return 'Check';
-    case 'cashiers_check':
-      return 'Cashiers Check';
-    case 'money_order':
-      return 'Money Order';
-    case 'comchek':
-      return 'Comchek';
-    case 'credit_card':
-      return 'Credit Card';
-    case 'direct_deposit':
-      return 'Direct Deposit';
-    case 'other':
-      return 'Other';
-    default:
-      return 'Unknown';
-  }
+  return values.get(value) ?? 'Unknown';
 }
--- src/PaymentTerm.ts
+++ src/PaymentTerm.ts
@@ -15,24 +15,33 @@ export type PaymentTerm =
   | '30_days'
   | '45_days';
 
+const values = new Map<PaymentTerm, string>([
+  ['other', 'Other'],
+  ['ach', 'ACH'],
+  ['comchek', 'Comchek'],
+  ['cash_on_pickup', 'Cash on Pickup'],
+  ['cash_on_delivery', 'Cash on Delivery'],
+  ['quick_pay', 'QuickPay'],
+  ['check_on_delivery', 'Check on Delivery'],
+  ['check_on_pickup', 'Check on Pickup'],
+  ['5_days', '5 Days'],
+  ['7_days', '7 Days'],
+  ['10_days', '10 Days'],
+  ['15_days', '15 Days'],
+  ['20_days', '20 Days'],
+  ['30_days', '30 Days'],
+  ['45_days', '45 Days'],
+]);
+
+const shortValues = new Map<PaymentTerm, string>([
+  ['cash_on_pickup', 'COP'],
+  ['cash_on_delivery', 'COD'],
+  ['check_on_delivery', 'CKOD'],
+  ['check_on_pickup', 'CKOP'],
+]);
+
 export function listPaymentTerms(): PaymentTerm[] {
-  return [
-    'other',
-    'ach',
-    'comchek',
-    'cash_on_pickup',
-    'cash_on_delivery',
-    'quick_pay',
-    'check_on_delivery',
-    'check_on_pickup',
-    '5_days',
-    '7_days',
-    '10_days',
-    '15_days',
-    '20_days',
-    '30_days',
-    '45_days',
-  ];
+  return Array.from(values.keys());
 }
 
 export interface FormatPaymentTermOptions {
@@ -43,39 +52,5 @@ export function formatPaymentTerm(
   value: PaymentTerm,
   { short = false }: FormatPaymentTermOptions = {},
 ): string {
-  switch (value) {
-    case 'other':
-      return 'Other';
-    case 'ach':
-      return 'ACH';
-    case 'comchek':
-      return 'Comchek';
-    case 'cash_on_pickup':
-      return short ? 'COP' : 'Cash on Pickup';
-    case 'cash_on_delivery':
-      return short ? 'COD' : 'Cash on Delivery';
-    case 'quick_pay':
-      return 'QuickPay';
-    case 'check_on_delivery':
-      return short ? 'CKOD' : 'Check on Delivery';
-    case 'check_on_pickup':
-      return short ? 'CKOP' : 'Check on Pickup';
-    case '5_days':
-      return '5 Days';
-    case '7_days':
-      return '7 Days';
-    case '10_days':
-      return '10 Days';
-    case '15_days':
-      return '15 Days';
-    case '20_days':
-      return '20 Days';
-    case '30_days':
-      return '30 Days';
-    case '45_days':
-      return '45 Days';
-
-    default:
-      return 'Unknown';
-  }
+  return ((short && shortValues.get(value)) || values.get(value)) ?? 'Unknown';
 }
