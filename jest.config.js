4433fe5fe3196c8ff1d17234736fbebd3060f656
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2020-07-20 10:23:33 +0500

    chore: [Jest] move config to the `package.json`.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/pkg/', '/node_modules/'],
-
-  // The glob patterns Jest uses to detect test files
-  testMatch: ['**/__tests__/**/*.spec.ts'],
-};
